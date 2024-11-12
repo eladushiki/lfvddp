545f074fbf58fe0e78592b23848cc75679402995
Elad Kliger <elad.kliger@gmail.com>
2024-11-12 16:05:31 +0200

    Still not sure what's going on when submitting, but some stuff is changed there

--- train/submit_train.py
+++ train/submit_train.py
@@ -1,38 +1,44 @@
 from argparse import ArgumentParser
 from pathlib import Path
+from time import sleep
 
-from frame.config_handle import version_controlled_execution_context
+from frame.command_line_args import context_controlled_execution
+from frame.config_handle import ExecutionContext, version_controlled_execution_context
+from frame.submit import prepare_submit_files_save, submit_save_jobs
 from train.train_config import TrainConfig
 
-def train(cluster_config_path: Path, train_config_path: Path) -> None:
-    config = TrainConfig.load_from_files([cluster_config_path, train_config_path])
-    with version_controlled_execution_context(config) as context:
-        submit_train_job(config)  # todo: how to differentiate when running train from other stuff here?
+@context_controlled_execution
+def train(context: ExecutionContext) -> None:
+    if not isinstance(context.config, TrainConfig):
+        raise ValueError("Expected TrainConfig, got {context.config.__class__.__name__}")
+    submit_train_job(context.config)
+
 
 def submit_train_job(train_config: TrainConfig) -> None:
     out_dir = train_config.out_dir
     runtag = train_config.runtag
+    jobid = 1  # ad hoc
     
-    setupLines=[f"cd {out_dir}"]
-    fsubname=f"{out_dir}/{runtag}/submit/sub_{jobid}_{i}_{number_of_signals}_{TAU_or_delta}_{sample}_{pdf}.sh"
-    cmd_TAU=f"/usr/local/anaconda/3.8/bin/python {scriptsdir}/new_training.py -j {jobid} -R {train_size} -B {test_size} -s {number_of_signals} -c {config_path} -o {outdir}/training_outcomes --seed {seed} "
-    cmd_TAU+=f"-t TAU -S {sample} --BDstr Bkg --SDstr Sig --BAstr Ref+Bkg+Sig --smlpdf {pdf}"
-    u.prepare_submit_file(fsubname,setupLines,[cmd_TAU],shortname=f"sub_{jobid}_{i}_{number_of_signals}_TAU_{sample}_{pdf}",setupATLAS=False)
-    u.submit_job(fsubname,walltime=walltime[i],io=0,mem=4)
+    # setupLines=[f"cd {out_dir}"]
+    # fsubname=f"{out_dir}/{runtag}/submit/sub_{jobid}_{i}_{number_of_signals}_{TAU_or_delta}_{sample}_{pdf}.sh"
+    # cmd_TAU=f"/usr/local/anaconda/3.8/bin/python {scriptsdir}/new_training.py -j {jobid} -R {train_size} -B {test_size} -s {number_of_signals} -c {config_path} -o {out_dir}/training_outcomes --seed {seed} "
+    # cmd_TAU+=f"-t TAU -S {sample} --BDstr Bkg --SDstr Sig --BAstr Ref+Bkg+Sig --smlpdf {pdf}"
+    # u.prepare_submit_file(fsubname, setupLines,[cmd_TAU], shortname=f"sub_TAU", setupATLAS=False)
+    # u.submit_job(fsubname,walltime=walltime[i],io=0,mem=4)
 
-    fsubname=f"{outdir}/{runtag}"+"/submit/"+f"sub_{jobid}_{i}_{number_of_signals}_delta_{sample}_{pdf}.sh"
-    cmd_delta=f"/usr/local/anaconda/3.8/bin/python {scriptsdir}/new_training.py -j {jobid} -R {train_size} -B {test_size} -s {number_of_signals} -c {config_path} -o {outdir}/training_outcomes --seed {seed} "
-    cmd_delta+=f"-t delta -S {sample} --BDstr Ref --BAstr Ref+Bkg+Sig --smlpdf {pdf}"
-    u.prepare_submit_file(fsubname,setupLines,[cmd_delta],shortname=f"sub_{jobid}_{i}_{number_of_signals}_delta_{sample}_{pdf}",setupATLAS=False)
-    u.submit_job(fsubname,walltime=walltime[i],io=0,mem=4)
+    # fsubname=f"{out_dir}/{runtag}"+"/submit/"+f"sub_{jobid}_{i}_{number_of_signals}_delta_{sample}_{pdf}.sh"
+    # cmd_delta=f"/usr/local/anaconda/3.8/bin/python {train_config.scripts_dir}/new_training.py -j {jobid} -R {train_size} -B {test_size} -s {number_of_signals} -c {config_path} -o {out_dir}/training_outcomes --seed {seed} "
+    # cmd_delta+=f"-t delta -S {train_config.train__histogram_analytic_pdf} --BDstr Ref --BAstr Ref+Bkg+Sig --smlpdf {pdf}"
+    # u.prepare_submit_file(fsubname, setupLines, [cmd_delta], shortname=f"sub_delta", setupATLAS=False)
+    # u.submit_job(fsubname,walltime=train_config.walltime[i],io=0,mem=4)
 
-    fsubname=f"{outdir}/{runtag}"+"/submit/"+f"sub_save_{i}_{number_of_signals}.sh"
-    setupLines=[f"cd {outdir}"]
-    cmd_to_save=f"/usr/local/anaconda/3.8/bin/python {scriptsdir}/new_make_jobs_tar+csv.py {train_size} {test_size} {number_of_signals} {N_jobs} {remove} {config_path} {outdir}/training_outcomes {sample} {pdf} {scriptsdir}/training_outcomes"
-    time.sleep(30)
-    jobs.prepare_submit_files_save(fsubname,setupLines,[cmd_to_save],shortname=f"sub_save_{i}_{number_of_signals}",setupATLAS=False)
-    N_jobs_wait = N_jobs+N_jobs
-    jobs.submit_save_jobs(fsubname,N_jobs_wait,walltime=save_walltime,io=0,mem=2)
+    # fsubname=f"{out_dir}/{runtag}"+"/submit/"+f"sub_save.sh"
+    # setupLines=[f"cd {out_dir}"]
+    # cmd_to_save=f"/usr/local/anaconda/3.8/bin/python {train_config.scripts_dir}/new_make_jobs_tar+csv.py"
+    # sleep(30)
+    # prepare_submit_files_save(fsubname, setupLines, [cmd_to_save], shortname=f"sub_save", setupATLAS=False)
+    # N_jobs_wait = train_config.n_jobs * 2
+    # submit_save_jobs(fsubname, N_jobs_wait,walltime=train_config.save_walltime, io=0, mem=2)
 
 
 def main():
