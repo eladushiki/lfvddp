693f05e564b574b6dd1c4338c5c31c3b9927a06b
Elad Kliger <elad.kliger@weizmann.ac.il>
2025-02-20 23:48:27 +0200

    Updated aggregation to include origin files

--- train/aggregate_train_results.py
+++ train/aggregate_train_results.py
@@ -2,15 +2,16 @@ from argparse import ArgumentParser
 from glob import glob
 from logging import warning
 from pathlib import Path
-from typing import List
-from frame.file_structure import AGGREGATED_TRAINING_RESULTS_FILE_NAME, SINGLE_TRAINING_RESULT_POSTFIX
+from typing import List, Tuple
+from csv import writer as csv_writer
+from frame.file_structure import AGGREGATED_TRAINING_RESULTS_FILE_NAME, SINGLE_TRAINING_RESULT_FILE_EXTENSION
 
 
-def aggregate_train_results(parent_directory: Path) -> List[float]:
+def aggregate_train_results(parent_directory: Path) -> List[Tuple[float, str]]:
     if not parent_directory.is_dir():
         raise NotADirectoryError(f"Parent directory {parent_directory} does not exist")
     
-    files_in_output_dir = glob(str(parent_directory) + f"/**/*.{SINGLE_TRAINING_RESULT_POSTFIX}", recursive=True)
+    files_in_output_dir = glob(str(parent_directory) + f"/**/*.{SINGLE_TRAINING_RESULT_FILE_EXTENSION}", recursive=True)
     
     # Append all legal files
     aggregated_results = []
@@ -18,7 +19,7 @@ def aggregate_train_results(parent_directory: Path) -> List[float]:
         try:
             with open(file, 'r') as f:
                 content = f.read()
-            result = float(content)
+            result = (float(content), file)
         except ValueError:
             warning(f"Could not parse training result from file {file}")
             continue
@@ -35,6 +36,8 @@ if __name__ == "__main__":
     train_results = aggregate_train_results(args.parent_directory)
 
     # todo: this also should be run in a context, yet requires different args
-    
-    with open(args.parent_directory / AGGREGATED_TRAINING_RESULTS_FILE_NAME, 'w') as f:
-        f.writelines([f"{num}\n" for num in train_results])
+
+    with open(args.parent_directory / AGGREGATED_TRAINING_RESULTS_FILE_NAME, 'w', newline='\n') as csvfile:
+        csvwriter = csv_writer(csvfile)
+        # csvwriter.writerow(['Result', 'File'])  # todo: wrap with common function to ensure headers are ok
+        csvwriter.writerows(train_results)
