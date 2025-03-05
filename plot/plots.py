43172317f1b31fd0d27fa8218074344dd77c2200
Elad Kliger <elad.kliger@weizmann.ac.il>
2025-03-05 16:27:03 +0200

    Plot title correction

--- plot/plots.py
+++ plot/plots.py
@@ -124,7 +124,8 @@ def plot_old_t_distribution(
     # plot distribution histogram
     bins      = np.linspace(xmin, xmax, number_of_bins + 1)
     bin_width = (xmax - xmin) * 1./number_of_bins
-    label     = f"median: {str(np.around(np.median(t), 2))} \nstd: {str(np.around(np.std(t), 2))}"
+    label     = f"median: {str(np.around(np.median(t), 2))} \n" \
+                f"std: {str(np.around(np.std(t), 2))}"
     
     h = ax.hist(
         t,
@@ -174,8 +175,8 @@ def plot_old_t_distribution(
     )
     
     # Texting
-    number_of_test_events = scientific_number(config.train__batch_test_fraction * results.N)
-    number_of_background_events = scientific_number(config.train__number_of_background_events * results.N)
+    number_of_test_events = scientific_number(config.train__number_of_reference_events + config.train__signal_number_of_events)
+    number_of_background_events = scientific_number(config.train__number_of_background_events)
     histogram_title = r'$N_A^0=$'+f"{number_of_test_events}"+r',   $N_B^0=$'+f"{number_of_background_events}"
     ax.set_title(histogram_title, fontsize=30, pad=20)
     ax.set_xlabel('t', labelpad=20)
