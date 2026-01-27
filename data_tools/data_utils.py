Normalization for training fixes

data_tools/data_utils.py
@@ -55,32 +55,36 @@ class DataSet:
         result._weight_mask = _weight_mask
         return result
     
-    def __mul__(self, other: DatasetNormalizationFactor) -> DataSet:
-        assert isinstance(other, DatasetNormalizationFactor), \
-            f"Dataset multiplication is only allowed by a DatasetNormalizationFactor, not {type(other)}"
+    def __mul__(self, other: ShiftAndNormalizationFactor) -> DataSet:
+        assert isinstance(other, ShiftAndNormalizationFactor), \
+            f"Dataset multiplication is only allowed by a ShiftAndNormalizationFactor, not {type(other)}"
 
         result = self.create_copy()        
         for obs in self.observable_names:
             try:
+                native_offset = min(result._data[obs])
+                result._data[obs] -= native_offset
                 result._data[obs] *= other.get_factor(obs)
-                result._data[obs] += other.get_offset(obs)
+                result._data[obs] += other.get_offset(obs) + native_offset
             except KeyError:
                 raise ArithmeticError(f"No factor for observable {obs} in multiplication")
             
         return result
 
-    def __rmul__(self, other: DatasetNormalizationFactor) -> DataSet:
+    def __rmul__(self, other: ShiftAndNormalizationFactor) -> DataSet:
         return self.__mul__(other)
 
-    def __truediv__(self, other: DatasetNormalizationFactor) -> DataSet:
-        assert isinstance(other, DatasetNormalizationFactor), \
-            f"Dataset division is only allowed by a DatasetNormalizationFactor, not {type(other)}"
+    def __truediv__(self, other: ShiftAndNormalizationFactor) -> DataSet:
+        assert isinstance(other, ShiftAndNormalizationFactor), \
+            f"Dataset division is only allowed by a ShiftAndNormalizationFactor, not {type(other)}"
         
         result = self.create_copy()
         for obs in self.observable_names:
             try:
-                result._data[obs] -= other.get_offset(obs)
+                native_offset = min(result._data[obs])
+                result._data[obs] -= native_offset
                 result._data[obs] /= other.get_factor(obs)
+                result._data[obs] -= other.get_offset(obs) - native_offset
             except KeyError:
                 raise ArithmeticError(f"No factor for observable {obs} in division")
             
@@ -146,17 +150,19 @@ class DataSet:
         except KeyError as e:
             raise KeyError(f"One or more observable names not found in dataset: {observables}") from e
     
-    def get_normalized(self) -> Tuple[DataSet, DatasetNormalizationFactor]:
+    def get_normalized(self) -> Tuple[DataSet, ShiftAndNormalizationFactor]:
         offsets = {}
         factors = {}
         result = self.create_copy()
         for obs in result.observable_names:
             obs_slice = result.slice_along_observable_names(obs)
             
-            offsets[obs] = min(obs_slice)
-            factors[obs] = max(obs_slice) - offsets[obs]
+            # shift and scale to fit range [-1, 1]
+            offsets[obs] = min(obs_slice) + 1
+            span = max(obs_slice) - min(obs_slice)
+            factors[obs] = span / 2
 
-        normalization_factor = DatasetNormalizationFactor(factors, offsets)
+        normalization_factor = ShiftAndNormalizationFactor(factors, offsets)
         return result / normalization_factor, normalization_factor
 
     def filter(self, filter: np.ndarray) -> DataSet:
@@ -212,7 +218,7 @@ def resample(
 
 
 @dataclass
-class DatasetNormalizationFactor:
+class ShiftAndNormalizationFactor:
     """
     Normalization factor to be applied to datasets for training and fitting.
     """

test/test_datasets.py
@@ -15,15 +15,16 @@ def test_dataset_normalization(
     function_execution_context,
     data_generation,
 ):
-    A, A_params = data_generation["A"]
-    B, B_params = data_generation["B"]
+    A, _ = data_generation["A"]
+    B, _ = data_generation["B"]
 
     normalized_A, norm_factor_A = A.get_normalized()
     normalized_B, norm_factor_B = B.get_normalized()
 
-    assert np.max(normalized_A.events) == 1
-    assert np.max(normalized_B.events) == 1
-    assert np.min(normalized_A.events) == 0
-    assert np.min(normalized_B.events) == 0
+    np.testing.assert_allclose(np.max(normalized_A.events), 1, rtol=1e-9, atol=1e-9)
+    np.testing.assert_allclose(np.max(normalized_B.events), 1, rtol=1e-9, atol=1e-9)
+    np.testing.assert_allclose(np.min(normalized_A.events), -1, rtol=1e-9, atol=1e-9)
+    np.testing.assert_allclose(np.min(normalized_B.events), -1, rtol=1e-9, atol=1e-9)
 
-    assert all((normalized_A * norm_factor_A).events == A.events)
+    np.testing.assert_array_almost_equal((normalized_A * norm_factor_A).events, A.events)
+    np.testing.assert_array_almost_equal((normalized_B * norm_factor_B).events, B.events)