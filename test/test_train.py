5363b7a4304d41b033e85300d8b18b39086b1c52
Elad Kliger <elad.kliger@weizmann.ac.il>
2026-07-05 09:45:50 +0300

    Update test/test_train.py

--- test/test_train.py
+++ test/test_train.py
@@ -68,7 +68,7 @@ def test_convergence(
     data_generation,
     detector_effect,
 ):
-    A, A_params = data_generation[DataSet.DataSetCategory.A]
+    A, A_params = data_generation[DataSet.DataSetCategory.A_SR]
     B, B_params = data_generation[DataSet.DataSetCategory.B_SR]
 
     affected_A = detector_effect.affect_and_compensate(A, A_params, True)
