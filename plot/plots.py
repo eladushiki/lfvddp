f6d23baf4415a403a3c58f9b2ab7d1a31fecda0c
Elad Kliger <elad.kliger@gmail.com>
2026-09-02 01:16:34 +0300

    Prevent detector binning name drift in prediction plots

--- plot/plots.py
+++ plot/plots.py
@@ -781,7 +781,7 @@ def _prediction_spanning_dataset(
         else {
             observable_name: detector_effect.get_observable_bins(observable_name)
             for observable_name in configured_observables
-            if observable_name in detector_effect.observable_names
+            if observable_name in detector_effect.binned_observable_names
         }
     )
 
